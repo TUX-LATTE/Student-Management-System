6e12312e55bd0189ca286242f1e6e80839b244d6
TUX-LATTE <penguinlatte.yassien@gmail.com>
2025-09-22 20:04:50 +0300

    Part 1: Validating Input

--- main.c
+++ main.c
@@ -7,25 +7,42 @@ int main(void)
 
     int studentID = 0;
 
-    int mathsGrade = 0; 
-    int scienceGrade = 0; 
-    int englishGrade = 0;
+    unsigned int mathsGrade = 0; 
+    unsigned int scienceGrade = 0; 
+    unsigned int englishGrade = 0;
 
     printf("%s", "Enter your ID: ");
-    scanf("%d", &studentID);
+    scanf("%d"
+    , &studentID);
 
     printf("%s", "Enter the Maths grade: ");
     scanf("%d", &mathsGrade);
 
+    if(mathsGrade < 0 || mathsGrade > 100)
+    {
+        puts("Out of expected range input, Try again later!");
+        return 1;
+    }
+    
     printf("%s", "Enter the Science grade: ");
     scanf("%d", &scienceGrade);
+
+    if(scienceGrade < 0 || scienceGrade > 100)
+    {
+        puts("Out of expected range input, Try again later!");
+        return 1;
+    }
 
     printf("%s", "Enter the English grade: ");
     scanf("%d", &englishGrade);
 
-    printf("Student ID: %d\nMaths: %d\tScience: %d\tEnglish: %d\t\n", studentID, mathsGrade, scienceGrade, englishGrade);
+    if(englishGrade < 0 || englishGrade > 100)
+    {
+        puts("Out of expected range input, Try again later!");
+        return 1;
+    }
 
-    
+    printf("Student ID: %d\nMaths: %d\tScience: %d\tEnglish: %d\t\n", studentID, mathsGrade, scienceGrade, englishGrade);
 
     return 0;
 }
