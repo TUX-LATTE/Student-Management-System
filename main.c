84caa8f4ba3d13be615c4c120ebdea58307ffd0e
TUX-LATTE <penguinlatte.yassien@gmail.com>
2025-09-26 16:58:55 +0300

    Part 1: Finished!

--- main.c
+++ main.c
@@ -10,94 +10,103 @@ int main(void)
     unsigned int scienceGrade = 0; 
     unsigned int englishGrade = 0;
 
-    printf("%s", "Enter your ID: ");
-    scanf("%d", &studentID);
+    int cont = 1;
 
-    printf("%s", "Enter the Maths grade: ");
-    scanf("%d", &mathsGrade);
 
-    if(mathsGrade < 0 || mathsGrade > 100)
+    while(cont)
     {
-        puts("Out of expected range input, Try again later!");
-        return 1;
-    }
+        printf("%s", "Enter your ID: ");
+        scanf("%d", &studentID);
     
-    printf("%s", "Enter the Science grade: ");
-    scanf("%d", &scienceGrade);
-
-    if(scienceGrade < 0 || scienceGrade > 100)
-    {
-        puts("Out of expected range input, Try again later!");
-        return 1;
-    }
-
-    printf("%s", "Enter the English grade: ");
-    scanf("%d", &englishGrade);
-
-    if(englishGrade < 0 || englishGrade > 100)
-    {
-        puts("Out of expected range input, Try again later!");
-        return 1;
-    }
-
-
-    printf("Student ID: %d\nMaths: %d\tScience: %d\tEnglish: %d\t\n", studentID, mathsGrade, scienceGrade, englishGrade);
+        printf("%s", "Enter the Maths grade: ");
+        scanf("%d", &mathsGrade);
     
-
-    int totalGrades = mathsGrade + scienceGrade + englishGrade;
-    double average = (double) totalGrades / 3;
-
-
-    int highestGrade = mathsGrade;
+        if(mathsGrade < 0 || mathsGrade > 100)
+        {
+            puts("Out of expected range input, Try again later!");
+            return 1;
+        }
+        
+        printf("%s", "Enter the Science grade: ");
+        scanf("%d", &scienceGrade);
     
-    if(highestGrade < scienceGrade)
-    {
-        highestGrade = scienceGrade;
-    }
+        if(scienceGrade < 0 || scienceGrade > 100)
+        {
+            puts("Out of expected range input, Try again later!");
+            return 1;
+        }
     
-    if(highestGrade < englishGrade)
-    {
-        highestGrade = englishGrade;
-    }
-
+        printf("%s", "Enter the English grade: ");
+        scanf("%d", &englishGrade);
     
-    int lowestGrade = mathsGrade;
-
-    if(lowestGrade > scienceGrade)
-    {   
-        lowestGrade = scienceGrade;
-    }
+        if(englishGrade < 0 || englishGrade > 100)
+        {
+            puts("Out of expected range input, Try again later!");
+            return 1;
+        }
     
-    if(lowestGrade > englishGrade)
-    {
-        lowestGrade = englishGrade;
-    }
-
-
-    printf("Highest: %d\t Lowest: %d\n", highestGrade, lowestGrade);
-
-
-    printf("%s", "Performance: ");
-    if(average >= 90)
-    {
-        printf("%s", "Excellent!");
-    }
-    else if(average >= 80)
-    {
-        printf("%s", "Very Good");
-    }
-    else if(average >= 70)
-    {
-        printf("%s", "Good");
-    }
-    else if(average >= 60)
-    {
-        printf("%s", "Pass");
-    }
-    else if(average < 60)
-    {
-        printf("%s", "Fail!");
-    }
     
+        printf("Student ID: %d\nMaths: %d\tScience: %d\tEnglish: %d\t\n", studentID, mathsGrade, scienceGrade, englishGrade);
+        
+    
+        int totalGrades = mathsGrade + scienceGrade + englishGrade;
+        double average = (double) totalGrades / 3;
+    
+    
+        int highestGrade = mathsGrade;
+        
+        if(highestGrade < scienceGrade)
+        {
+            highestGrade = scienceGrade;
+        }
+        
+        if(highestGrade < englishGrade)
+        {
+            highestGrade = englishGrade;
+        }
+    
+        
+        int lowestGrade = mathsGrade;
+    
+        if(lowestGrade > scienceGrade)
+        {   
+            lowestGrade = scienceGrade;
+        }
+        
+        if(lowestGrade > englishGrade)
+        {
+            lowestGrade = englishGrade;
+        }
+    
+    
+        printf("Highest: %d\t Lowest: %d\n", highestGrade, lowestGrade);
+    
+    
+        printf("%s", "Performance: ");
+        if(average >= 90)
+        {
+            printf("%s", "Excellent!");
+        }
+        else if(average >= 80)
+        {
+            printf("%s", "Very Good");
+        }
+        else if(average >= 70)
+        {
+            printf("%s", "Good");
+        }
+        else if(average >= 60)
+        {
+            printf("%s", "Pass");
+        }
+        else if(average < 60)
+        {
+            printf("%s", "Fail!");
+        }
+
+        
+        printf("\nWant to continue(Any Key: Yes/0: No)?: ");
+        scanf("%d", &cont);
+    }
     return 0;
 }
